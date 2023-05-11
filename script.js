3577cbb79879e37111b710979a732101399d41a0
Christopher Erb <christophererb01@gmail.com>
2023-05-11 10:15:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+/* functionName () {}
+
+function helloWorld ()
+{
+    console.log('hello world')
+}
+
+//helloWorld()
+
+
+We put paramaters in the ()
+
+
+function sayHelloName (name) 
+{
+
+
+    //We call parameters in the action
+console.log(`hello, ${name}`)
+
+}
+
+sayHelloName("chris")
+sayHelloName("Melanie")
+sayHelloName("Chris two")
+sayHelloName("Melanie twos")
+sayHelloName("Chris 3")
+sayHelloName("Melanie three")
+
+
+
+
+function sayHelloFullName (firstname, lastname, age)
+{
+console.log(`Hello ${firstname} ${lastname}. I see that you are ${age} years old.`)
+}
+
+sayHelloFullName("Christopher", "Erb" , 36) 
+*/
+
+
+function addLiterals(l1, l2)
+{
+    console.log(`can you add literals in here?  ${l1 + l2}`)
+
+}
+
+addLiterals(35,4.2)
+
